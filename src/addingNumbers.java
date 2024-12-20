5eaf916f5b7db598bdbfe0f6e323ed5144f114d5
Aniket7879 <gangurdeaniket083@gmail.com>
2024-12-20 16:14:14 +0530

    adding Numbers code

---
+++ src/addingNumbers.java
@@ -0,0 +1,19 @@
+public class addingNumbers {
+
+    public static void main(String args[]){
+
+        System.out.println("Hello Aniket");
+
+        System.out.println("Hello Zoro");
+
+        int no1 = 7;
+        int no2 = 8;
+        int no3 = 9;
+
+        int sum = no1 + no2 + no3;
+
+        System.out.println("The Addition for the above 3 numbers is : " + sum);
+
+    }
+
+}
