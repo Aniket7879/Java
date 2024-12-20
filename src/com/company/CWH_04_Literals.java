154b4ff89f4a3070c2e11c1499f4d629eb6a183d
Aniket7879 <gangurdeaniket083@gmail.com>
2024-12-20 17:53:11 +0530

    Java Literals

---
+++ src/com/company/CWH_04_Literals.java
@@ -0,0 +1,29 @@
+package com.company;
+
+public class CWH_04_Literals {
+
+    public static void main(String []args){
+
+        System.out.println("Literals : ");
+
+        byte age = 23;
+        int i = 34;
+        short s = 12;
+        long L = 223;
+        long L1 = 223535949359l;
+        long L2 = 223424332324L;
+        char ch = 'A';
+        float f = 8.8F;
+        float f1 = 8.8f;
+        double d = 2.6;
+        double d1 = 22.6d;
+        double d2 = 223.678D;
+        boolean b = true;
+        boolean b1 = false;
+        String str =  "Aniket";
+
+        System.out.println(str);
+
+    }
+
+}
